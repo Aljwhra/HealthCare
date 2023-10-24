8a6b0499ea730b39beaf57516a24fd3f2fe1e3d7
Aljwhra <aljwhra.n2@icloud.com>
2023-10-24 10:03:10 +0300

    add

---
+++ Healthcare/Authentication/LogIn_Success.swift
@@ -0,0 +1,18 @@
+//
+//  LogIn_Success.swift
+//  Healthcare
+//
+//  Created by Aljwhra Alnasser on 24/10/2023.
+//
+
+import SwiftUI
+
+struct LogIn_Success: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LogIn_Success()
+}
